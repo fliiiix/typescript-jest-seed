3d6cbf7cf329a4acb327e5983693ffea28347062
fliiiix <hi@l33t.name>
2020-02-04 08:34:08 +0100

    v1

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,28 @@
-export default class School {
-  
-  //Your code goes here
+export default class School {  
+ private students: Map<string, Array<string>> = new Map();
 
+  classlist() : Map<string, Array<string>> {
+      let tmpList = new Map<string, Array<string>>();
+      this.students.forEach((value, key) => { tmpList.set(key, [...value]) })
+      return tmpList;
+  }
+
+  addStudent(name: string, klasse: number) : void {
+      let k = klasse.toString();
+      if(!this.students.has(k)){
+          this.students.set(k, [name]);
+      }
+      else{
+          this.students.get(k).push(name);
+          this.students.get(k).sort();
+      }
+  }
+
+  studentsInClass(klasse: number) : Array<string> {
+      let k = klasse.toString();
+      if (this.students.has(k)){
+          return [...this.students.get(k)];
+      }
+      return [];
+  }
 }
